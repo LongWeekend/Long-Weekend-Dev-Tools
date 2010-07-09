a8b3e6edbf29e1fc03b54b7760de7de36cd81eae
Mark Makdad <makdad@gmail.com>
2010-07-09 14:44:43 +1000

    Added generalized delete static method to LWECoreData

--- LWECoreData.h
+++ LWECoreData.h
@@ -14,9 +14,19 @@
 {  
 }
 
+//! Returns all entities of a given type from a given context (SELECT * FROM x)
 + (NSArray *) fetchAll:(NSString *)entityName managedObjectContext:(NSManagedObjectContext *)managedObjectContext;
+
+//! Returns entities of a given type from a given context with predicate (SELECT * FROM x WHERE y)
 + (NSArray *) fetch:(NSString *)entityName managedObjectContext:(NSManagedObjectContext *)managedObjectContext withSortDescriptors:(NSArray *)sortDescriptors predicate:(id)stringOrPredicate, ...;
+
+//! Returns entities of a given type from a given context with predicate & limit (SELECT * FROM x WHERE y LIMIT z)
 + (NSArray *) fetch:(NSString *)entityName managedObjectContext:(NSManagedObjectContext *)managedObjectContext withSortDescriptors:(NSArray *)sortDescriptorsOrNil withLimit:(int)limitOrNil predicate:(id)stringOrPredicate, ...;
+
+//! Saves the current context
 + (void) save:(NSManagedObjectContext *)managedObjectContext;
+
+//! Deletes the provided entity from the provided context
++ (BOOL) delete:(id)entity fromContext:(NSManagedObjectContext *)context;
 
 @end
--- LWECoreData.m
+++ LWECoreData.m
@@ -1,6 +1,5 @@
 //
 //  LWECoreData.m
-//  Rikai
 //
 //  Created by シャロット ロス on 6/13/10.
 //  Copyright 2010 LONG WEEKEND INC. All rights reserved.
@@ -26,6 +25,7 @@
   return [LWECoreData fetch:entityName managedObjectContext:managedObjectContext withSortDescriptors:nil predicate:nil];
 }
 
+
 /**
  * Gets all entities for a given entity & context ("SELECT * FROM foo WHERE x ORDER BY y" in SQL)
  * \param entityName Name of the Core Data entity to fetch
@@ -33,47 +33,20 @@
  * \param sortDescriptorsOrNil Sort descriptor, or use nil if you don't want to sort
  * \param predicate the "where clause" of the query
  */
-//TODO: make this reuse the fetch below.  I don't know how to pass the variadic arguments
 + (NSArray *) fetch:(NSString *)entityName managedObjectContext:(NSManagedObjectContext *)managedObjectContext withSortDescriptors:(NSArray *)sortDescriptorsOrNil predicate:(id)stringOrPredicate, ...
 {
-  NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
-  NSEntityDescription *entity = [NSEntityDescription entityForName:entityName inManagedObjectContext:managedObjectContext];
-  [fetchRequest setEntity:entity];
-  
-  if (sortDescriptorsOrNil != nil)
-  {
-    [fetchRequest setSortDescriptors:sortDescriptorsOrNil];
-  }
-  
-  // add the predicate (Apple-speak for where)
-  if (stringOrPredicate)
-  {
-    NSPredicate *predicate;
-    if ([stringOrPredicate isKindOfClass:[NSString class]])
-    {
-      va_list variadicArguments;
-      va_start(variadicArguments, stringOrPredicate);
-      predicate = [NSPredicate predicateWithFormat:stringOrPredicate
-                                         arguments:variadicArguments];
-      va_end(variadicArguments);
-    }
-    else
-    {
-      NSAssert2([stringOrPredicate isKindOfClass:[NSPredicate class]], @"Second parameter passed to %s is of unexpected class %@", sel_getName(_cmd), [stringOrPredicate class]);
-      predicate = (NSPredicate *)stringOrPredicate;
-    }
-    [fetchRequest setPredicate:predicate];
-  }
-  
-  NSError *error;
-  NSArray *results = [managedObjectContext executeFetchRequest:fetchRequest error:&error];
-  
-  [fetchRequest release]; 
-  
-  return results;
+  return [LWECoreData fetch:entityName managedObjectContext:managedObjectContext withSortDescriptors:sortDescriptorsOrNil withLimit:0 predicate:stringOrPredicate];
 }
 
-//! Same as the other fetch but takes a limit parameter
+
+/**
+ * Gets all entities for a given entity & context ("SELECT * FROM foo WHERE x ORDER BY y LIMIT z" in SQL)
+ * \param entityName Name of the Core Data entity to fetch
+ * \param managedObjectContext Which ObjectContext to use
+ * \param sortDescriptorsOrNil Sort descriptor, or use nil if you don't want to sort
+ * \param limitOrNil Integer number to limit by.  0 for no limit clause
+ * \param predicate the "where clause" of the query
+ */
 + (NSArray *) fetch:(NSString *)entityName managedObjectContext:(NSManagedObjectContext *)managedObjectContext withSortDescriptors:(NSArray *)sortDescriptorsOrNil withLimit:(int)limitOrNil predicate:(id)stringOrPredicate, ...
 {
   NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
@@ -124,12 +97,31 @@
  */
 + (void) save:(NSManagedObjectContext *)managedObjectContext
 {
-  //persist this
   NSError *error;
   if (![managedObjectContext save:&error]) 
   {
-    NSLog(@"This is embarrassing. We failed to save because: %@", [error localizedDescription]);
+    NSAssert2(0, @"This is embarrassing. We failed to save because: %@", [error localizedDescription]);
   }  
+}
+
+/**
+ * Deletes an entity from the current objectContext
+ * \param entity Object to delete
+ * \param context managedObjectContext to delete from
+ * \return returns YES on success, NO on delete failure (swallows exception)
+ */
++ (BOOL) delete:(id)entity fromContext:(NSManagedObjectContext *)context
+{
+	[context deleteObject:entity];
+  @try
+  {
+    [LWECoreData save:context];
+  }
+  @catch (NSException * e)
+  {
+    return NO;
+  }
+  return YES;
 }
 
 @end
