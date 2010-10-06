f02d35927a70c31d988278b57ad613b7bb2a56a3
pchapman <paul@longweekendmobile.com>
2010-10-06 13:14:33 +1100

    Updated withworking vertical centering

--- UIKit/LWELoadingView.m
+++ UIKit/LWELoadingView.m
@@ -25,81 +25,59 @@
 
 + (id)loadingView:(UIView *)aSuperview withText:(NSString *)text calculateNavigationBar:(BOOL)calculateNavigationBar
 {	
-	//STEP 1 - Initialise itself
-	CGRect selfFrame = [[UIScreen mainScreen] bounds]; //[aSuperview bounds];
-	LWELoadingView *loadingView = [[[LWELoadingView alloc] initWithFrame:selfFrame] autorelease];
+	CGRect borderFrame = [[UIScreen mainScreen] bounds]; //[aSuperview bounds];
+	LWELoadingView *loadingView = [[[LWELoadingView alloc] initWithFrame:borderFrame] autorelease];
 	if (!loadingView)
 	{
 		return nil;
 	}
 	
-	//STEP 2 - Define all of the constant
 	const CGSize DEFAULT_OFFSET_VIEW = CGSizeMake(DEFAULT_OFFSET_WIDTH, DEFAULT_OFFSET_HEIGHT);
+
+  // configure the label
 	CGRect labelFrame = CGRectMake(0, 0, DEFAULT_LABEL_WIDTH, DEFAULT_LABEL_HEIGHT);
 	UILabel *loadingLabel = [[UILabel alloc] initWithFrame:labelFrame];
 	loadingLabel.text = text;
   loadingLabel.numberOfLines = 3;
   loadingLabel.textColor = [UIColor whiteColor];
-	loadingLabel.backgroundColor = [UIColor clearColor];
+	loadingLabel.backgroundColor = [UIColor clearColor]; //[UIColor yellowColor];//
 	loadingLabel.textAlignment = UITextAlignmentCenter;
 	loadingLabel.font = [UIFont boldSystemFontOfSize:[UIFont labelFontSize]];
-	//loadingLabel.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | 
-	//																UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
-	//CGSize expectedLabelSize = [loadingLabel.text sizeWithFont:loadingLabel.font constrainedToSize:labelFrame.size lineBreakMode:loadingLabel.lineBreakMode];
-	//LWE_LOG(@"exp label size x %f, y %f",expectedLabelSize.width,expectedLabelSize.height);
-	[loadingView addSubview:loadingLabel];
 	
-	//STEP 3 - Create the activity indicator
-	UIActivityIndicatorView *activityIndicatorView = [[UIActivityIndicatorView alloc] 
-																										initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
-	//activityIndicatorView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin |	
-	//																				 UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
-	[loadingView addSubview:activityIndicatorView];
+	// configure the activity indicator
+	UIActivityIndicatorView *activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
 	[activityIndicatorView startAnimating];
-	
-	//STEP 4 - Fix the size of itself, x, y, width, and height as well. It is bassed on the total height of the loading label, and activity indicator
-	//Width is calculated based on the default width of the label with some addition of the default offset.
-	//Height is calculated based on the total height, with some addition of the default offset.
-	//X and Y are counted based on the total size of the superview bounds, minus the size of this loading view, divide by 2 (*0.5). 
+	[loadingView addSubview:activityIndicatorView];
+  
 	_totalHeight = loadingLabel.frame.size.height + activityIndicatorView.frame.size.height;
+
+	// size and center the loading view
+	borderFrame.size.width =  DEFAULT_LABEL_WIDTH + (DEFAULT_OFFSET_VIEW.width*2);
+	borderFrame.size.height = _totalHeight + (DEFAULT_OFFSET_VIEW.height*2);
+	borderFrame.origin.x = floor(0.5 * (loadingView.frame.size.width - borderFrame.size.width));
+	borderFrame.origin.y = floor((0.5 * loadingView.frame.size.height) - borderFrame.size.height);
+	loadingView.frame = borderFrame;
 	
-	selfFrame.size.width =  DEFAULT_LABEL_WIDTH + (DEFAULT_OFFSET_VIEW.width*2);
-	selfFrame.size.height = _totalHeight + (DEFAULT_OFFSET_VIEW.height*2);
-	selfFrame.origin.x = floor(0.5 * (loadingView.frame.size.width - selfFrame.size.width));
-	selfFrame.origin.y = floor(0.5 * (loadingView.frame.size.height - selfFrame.size.height));
-	loadingView.frame = selfFrame;
-	//If calculate navigation bar, means that the 0,0 location is below the navigation bar, so the y-coordinate origin
-	//has to be adjusted accordingly (44px is the default height for navigation bar)
-	//TODO: Check compatibility with iPhone 4
-	if (calculateNavigationBar)
-	{
-		selfFrame.origin.y = selfFrame.origin.y - 44.0;
-	}
-	LWE_LOG(@"The new loading view after resize x:%f, y:%f, width:%f, height:%f", loadingView.frame.origin.x, loadingView.frame.origin.y, loadingView.frame.size.width, loadingView.frame.size.height);	
-	
-	//STEP 5 - Fix the position of the label
-	labelFrame.origin.x = floor(0.5 * (loadingView.frame.size.width - DEFAULT_LABEL_WIDTH));
-	labelFrame.origin.y = floor(0.5 * (loadingView.frame.size.height - _totalHeight));
-	loadingLabel.frame = labelFrame;
-	
-	//STEP 6 - Fix the position of the activity indicator
-	CGRect activityIndicatorRect = activityIndicatorView.frame;
+  // position activity indicator
+  CGRect activityIndicatorRect = activityIndicatorView.frame;
 	activityIndicatorRect.origin.x = 0.5 * (loadingView.frame.size.width - activityIndicatorRect.size.width);
-	activityIndicatorRect.origin.y = loadingLabel.frame.origin.y + loadingLabel.frame.size.height;
+	activityIndicatorRect.origin.y = loadingLabel.frame.origin.y + loadingLabel.frame.size.height-5;
 	activityIndicatorView.frame = activityIndicatorRect;
-	
-	//STEP 7 - Clean up every memory space being used to.
+ 
+  // position the label
+	labelFrame.origin.x = floor(0.5 * (loadingView.frame.size.width - DEFAULT_LABEL_WIDTH));
+	labelFrame.origin.y = floor((0.5 * (loadingView.frame.size.height - _totalHeight)*0.1));
+  [loadingLabel sizeToFit];
+	loadingLabel.frame = labelFrame;
+  [loadingView addSubview:loadingLabel];
+  
 	[activityIndicatorView release];
 	[loadingLabel release];
 	
-	//STEP 8 - Fix itself with some additional set-up property.
 	loadingView.opaque = NO;
-	//Commented it out cause Rendy is not sure what it is for
-	//loadingView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 	[aSuperview addSubview:loadingView];
 	
-	//STEP 9 - Apply the fade-in animation.
-	// Set up the fade-in animation
+	// animate
 	CATransition *animation = [CATransition animation];
 	[animation setType:kCATransitionFade];
 	[[aSuperview layer] addAnimation:animation forKey:@"layerAnimation"];
