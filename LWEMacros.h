bde3c5d3a9f9f7ae3a1d3dc0c157e4ada2b28c5a
Ross Sharrott <rsharrott@gmail.com>
2011-02-08 15:18:25 +0900

    readded the UIColorFromHex macro

--- LWEMacros.h
+++ LWEMacros.h
@@ -17,3 +17,5 @@ else\
   NSLog(@"Delegate is not set or does not respond to SELECTOR");\
 }\
 } while(0)
+
+#define LWE_UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
