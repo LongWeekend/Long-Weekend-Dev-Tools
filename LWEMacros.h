681a1a79b8deaf9218ff6b5328219b78ece61442
Mark Makdad <makdad@gmail.com>
2011-02-08 16:32:59 +0900

    Merge error fix

--- LWEMacros.h
+++ LWEMacros.h
@@ -17,4 +17,4 @@ else\
 }\
 } while(0)
 
-#define LWE_UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0] } } while(0)
+#define LWE_UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
