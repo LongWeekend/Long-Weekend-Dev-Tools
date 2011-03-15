6d4ac3d94a15e303a5a4938dfcbb8604ce3e2f6a
Mark Makdad <makdad@gmail.com>
2011-03-15 20:00:13 +0900

    Added exception macro (instead of just assertion macro)

--- LWEDebug.h
+++ LWEDebug.h
@@ -7,17 +7,27 @@
  *
  */
 
-// Assertions
+// Assertions - for app store do nothing, otherwise assert!
 #if defined(LWE_RELEASE_APP_STORE)
   #define LWE_ASSERT(STATEMENT) do { (void) sizeof(STATEMENT); } while(0)
 #else
   #define LWE_ASSERT(STATEMENT) do { assert(STATEMENT); } while(0)
 #endif
 
+// Exceptions - for app store, do nothing, otherwise throw!
+#if defined(LWE_RELEASE_APP_STORE)
+  #define LWE_ASSERT_EXC(STATEMENT,MSG,...) do { (void) sizeof(STATEMENT); } while(0)
+#else
+  #define LWE_ASSERT_EXC(STATEMENT,MSG,...) do {\
+  if (!(STATEMENT)) {\
+  [NSException raise:NSGenericException format:MSG,## __VA_ARGS__];\
+  }\
+  } while (0)
+#endif
+
 // For dumping anything to the console
 #if defined(LWE_RELEASE_APP_STORE)
-#define LWE_LOG(format, ...);
+  #define LWE_LOG(format, ...);
 #else
-#define LWE_LOG(format, ...) NSLog(format, ## __VA_ARGS__);
-//  #define LWE_LOG(format, ...) NSLog([NSString stringWithFormat: format, ## __VA_ARGS__]);
+  #define LWE_LOG(format, ...) NSLog(format, ## __VA_ARGS__);
 #endif
