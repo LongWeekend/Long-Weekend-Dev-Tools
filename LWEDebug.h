3285067394df4ffdcfd1eba242e47c1d79b69f5d
Ross Sharrott <rsharrott@gmail.com>
2011-08-11 18:01:37 -0400

    fixed the build error because of the missing FlurryLib import
    Conflicts:
    
    	LWEDebug.h

--- LWEDebug.h
+++ LWEDebug.h
@@ -7,6 +7,8 @@
  *
  */
 
+#import "FlurryAPI.h"
+
 // Assertions - for app store do nothing, otherwise assert!
 #if defined(LWE_RELEASE_APP_STORE)
   #define LWE_ASSERT(STATEMENT) do { (void) sizeof(STATEMENT); } while(0)
