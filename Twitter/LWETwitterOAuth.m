353fa89430fe4325d422d5054ebe23da0973251a
Mark Makdad <makdad@gmail.com>
2011-11-30 16:33:05 +0900

    A bunch of style/whitespace changes

--- Twitter/LWETwitterOAuth.m
+++ Twitter/LWETwitterOAuth.m
@@ -46,39 +46,27 @@
 	return self;
 }
 
-#pragma mark -
-#pragma mark Start Authentication for the application key
+#pragma mark - Start Authentication for the application key
 
 //! This starts the Authentication phase with XAuth type. If its fails, its going to report back to its delegate. 
-- (void)startXAuthProcessWithUsername:(NSString *)uname 
-							 password:(NSString *)password
+- (void)startXAuthProcessWithUsername:(NSString *)uname password:(NSString *)password
 {
 	self.accessToken = nil;
-    OAMutableURLRequest *request = [self prepareURLForAuthType:LWET_ACCESS_TOKEN 
-													withTicket:nil 
-												  responseData:nil];
-    
-    [request setHTTPMethod:@"POST"];
+  OAMutableURLRequest *request = [self prepareURLForAuthType:LWET_ACCESS_TOKEN withTicket:nil responseData:nil];
+  [request setHTTPMethod:@"POST"];
     
 	//Prepare all of the param required for XAuth authentication type.
-    OARequestParameter *param1 = [[OARequestParameter alloc] 
-								  initWithName:@"x_auth_username" value:uname];
-	OARequestParameter *param2 = [[OARequestParameter alloc] 
-								  initWithName:@"x_auth_password" value:password];
-	OARequestParameter *param3 = [[OARequestParameter alloc] 
-								  initWithName:@"x_auth_mode" value:kClientAuth];
+  OARequestParameter *param1 = [[OARequestParameter alloc] initWithName:@"x_auth_username" value:uname];
+	OARequestParameter *param2 = [[OARequestParameter alloc] initWithName:@"x_auth_password" value:password];
+	OARequestParameter *param3 = [[OARequestParameter alloc] initWithName:@"x_auth_mode" value:kClientAuth];
 	
-    NSArray *params = [NSArray arrayWithObjects:param1, param2, param3, nil];
-    [request setParameters:params];
+  NSArray *params = [NSArray arrayWithObjects:param1, param2, param3, nil];
+  [request setParameters:params];
 	
-	OADataFetcher *fetcher = [[[OADataFetcher alloc] 
-							   init] autorelease];
-    
-    [fetcher fetchDataWithRequest:request
-                         delegate:self
-                didFinishSelector:@selector(didFinishXAuthWithTicket:data:)
-                  didFailSelector:@selector(didFailXAuthWithTicket:error:)];
-	
+	OADataFetcher *fetcher = [[[OADataFetcher alloc] init] autorelease];
+  [fetcher fetchDataWithRequest:request delegate:self
+              didFinishSelector:@selector(didFinishXAuthWithTicket:data:)
+                didFailSelector:@selector(didFailXAuthWithTicket:error:)];
 	[param1 release];
 	[param2 release];
 	[param3 release];
@@ -88,54 +76,39 @@
 - (void)startAuthProccess
 {	
 	self.accessToken = nil;
-    OAMutableURLRequest *request = [self prepareURLForAuthType:LWET_REQUEST 
-													withTicket:nil 
-												  responseData:nil];
+  OAMutableURLRequest *request = [self prepareURLForAuthType:LWET_REQUEST withTicket:nil responseData:nil];
     
-    [request setHTTPMethod:@"POST"];
+  [request setHTTPMethod:@"POST"];
     
-    OARequestParameter *param1 = [[OARequestParameter alloc] 
-								  initWithName:@"oauth_callback" value:@"oob"];
-    NSArray *params = [NSArray arrayWithObject:param1];
-    [request setParameters:params];
+  OARequestParameter *param1 = [[OARequestParameter alloc] initWithName:@"oauth_callback" value:@"oob"];
+  NSArray *params = [NSArray arrayWithObject:param1];
+  [request setParameters:params];
 	
-	OADataFetcher *fetcher = [[[OADataFetcher alloc] 
-							   init] autorelease];
+	OADataFetcher *fetcher = [[[OADataFetcher alloc] init] autorelease];
     
-    [fetcher fetchDataWithRequest:request
-                         delegate:self
-                didFinishSelector:@selector(didFinishRequestTokenWithTicket:data:)
-                  didFailSelector:@selector(didFinishRequestTokenWithTicket:error:)];
+  [fetcher fetchDataWithRequest:request delegate:self
+              didFinishSelector:@selector(didFinishRequestTokenWithTicket:data:)
+                didFailSelector:@selector(didFinishRequestTokenWithTicket:error:)];
 	[param1 release];
 }
 
-#pragma mark -
-#pragma mark FinishRequestToken
+#pragma mark - FinishRequestToken
 
 //! Finish the first phase of OAuth processes. Its now going to authenticate the user with the twitter server via view controller that is provided, or default view controller.
-- (void)didFinishRequestTokenWithTicket:(OAServiceTicket *)ticket 
-								   data:(NSData *)data
+- (void)didFinishRequestTokenWithTicket:(OAServiceTicket *)ticket data:(NSData *)data
 {
-	OAMutableURLRequest *request = [self prepareURLForAuthType:LWET_AUTHORIZE 
-													withTicket:ticket 
-												  responseData:data];
+	OAMutableURLRequest *request = [self prepareURLForAuthType:LWET_AUTHORIZE withTicket:ticket responseData:data];
 	NSArray *params = nil;
 	//set the parameter required for requesting 
 	//a user authentication
-	OARequestParameter *requestParam = [[OARequestParameter alloc]
-										initWithName:@"oauth_token" 
-										value:self.accessToken.key];
+	OARequestParameter *requestParam = [[OARequestParameter alloc] initWithName:@"oauth_token" value:self.accessToken.key];
 	params = [NSArray arrayWithObject:requestParam];
 	[request setParameters:params];
 	
 	//default user authentication view
 	if (authenticationView == nil)
 	{
-		LWETDefaultAuthenticationViewController *controller = 
-		[[LWETDefaultAuthenticationViewController alloc]
-		 initWithNibName:@"LWETDefaultAuthenticationViewController" 
-		 bundle:nil];
-		
+		LWETDefaultAuthenticationViewController *controller = [[LWETDefaultAuthenticationViewController alloc] initWithNibName:@"LWETDefaultAuthenticationViewController" bundle:nil];
 		self.authenticationView = controller;
 		[controller release];
 	}
@@ -165,8 +138,6 @@
 	
 	LWE_LOG(@"LOAD THE REQUEST");
 	[[authenticationView webView] loadRequest:request];
-	
-	//clean memory
 	[requestParam release];	
 }
 
@@ -204,15 +175,10 @@
 	[request setParameters:params];
 	
 	//request made to server to get the token for certain user
-	OADataFetcher *fetcher = [[[OADataFetcher alloc]
-							   init] autorelease];
-	[fetcher fetchDataWithRequest:request 
-						 delegate:self 
-				didFinishSelector:
-	 @selector(didFinishAccessTokenWithTicket:data:) 
-				  didFailSelector:
-	 @selector(didFailAccessTokenWithTicket:error:)];
-	
+	OADataFetcher *fetcher = [[[OADataFetcher alloc] init] autorelease];
+	[fetcher fetchDataWithRequest:request delegate:self 
+              didFinishSelector:@selector(didFinishAccessTokenWithTicket:data:) 
+                didFailSelector:@selector(didFailAccessTokenWithTicket:error:)];
 	[params release];
 	[param1 release];
 	[param2 release];
@@ -226,51 +192,49 @@
 	[self.delegate didFailAuthProcessWithError:nil];
 }
 
-#pragma mark -
-#pragma mark FinishAccessToken
+#pragma mark -  FinishAccessToken
 
 //! Finishes all of the authentication method, and gets back to the delegate. 
-- (void)didFinishAccessTokenWithTicket:(OAServiceTicket *)ticket 
-								  data:(NSData *)data
+- (void)didFinishAccessTokenWithTicket:(OAServiceTicket *)ticket data:(NSData *)data
 {
 	LWE_LOG(@"FINISHED ALL OF THE AUTH, RETURN BACK TO THE DELEGATE");
-	[self updateAccessTokenWithTicket:ticket 
-								 data:data];
+	[self updateAccessTokenWithTicket:ticket data:data];
 	if (ticket.didSucceed)
-		[self.delegate 
-		 didFinishAuthProcessWithAccessToken:[self accessToken]];
+  {
+		[self.delegate didFinishAuthProcessWithAccessToken:self.accessToken];
+  }
 	else 
-		[self.delegate
-		 didFailAuthProcessWithError:[[[NSError alloc] init] 
-										autorelease]];
+  {
+    // TODO: MMA This doesn't make any sense, there's no error info
+		[self.delegate didFailAuthProcessWithError:[[[NSError alloc] init] autorelease]];
+  }
 }
 
 
 //! Failed authentication with strange reason. (Most of the cases, is twitter fault).
-- (void)didFailAccessTokenWithTicket:(OAServiceTicket *)ticket 
-							   error:(NSError *)error
+- (void)didFailAccessTokenWithTicket:(OAServiceTicket *)ticket error:(NSError *)error
 {
 	//TODO: Have Proper Error Handling
 	LWE_LOG(@"%@", [error userInfo]);
 	[self.delegate didFailAuthProcessWithError:error];
 }
 
-#pragma mark FinishXAuthProcess
+#pragma mark - FinishXAuthProcess
 
 //! Finally, XAuth finishes. It only needs to report back to its delegate, saying that it finishes. 
-- (void)didFinishXAuthWithTicket:(OAServiceTicket *)ticket 
-								  data:(NSData *)data
+- (void)didFinishXAuthWithTicket:(OAServiceTicket *)ticket data:(NSData *)data
 {
 	LWE_LOG(@"FINISHED XAUTH, RETURN BACK TO THE DELEGATE");
-	[self updateAccessTokenWithTicket:ticket 
-								 data:data];
+	[self updateAccessTokenWithTicket:ticket data:data];
 	if (ticket.didSucceed)
-		[self.delegate 
-		 didFinishXAuthProcessWithAccessToken:[self accessToken]];
+  {
+		[self.delegate didFinishXAuthProcessWithAccessToken:[self accessToken]];
+  }
 	else 
-		[self.delegate
-		 didFailXAuthProcessWithError:[[[NSError alloc] init] 
-										autorelease]];
+  {
+    // TODO: MMA This doesn't make any sense, there's no error info
+		[self.delegate didFailXAuthProcessWithError:[[[NSError alloc] init] autorelease]];
+  }
 }
 
 //! XAuth did fail, it's going to asks user for putting their username and password again. Thats the most probable scenario.
@@ -282,13 +246,11 @@
 	[self.delegate didFailXAuthProcessWithError:error];
 }
 
-#pragma mark -
-#pragma mark Core Method
+#pragma mark - Core Method
 
 
 //! Should be private method, and it is used for updating the token, with the new ticket coming from Twitter server, and with the response-data.
-- (void)updateAccessTokenWithTicket:(OAServiceTicket *)ticket 
-							   data:(NSData *)data
+- (void)updateAccessTokenWithTicket:(OAServiceTicket *)ticket data:(NSData *)data
 {
 	LWE_LOG(@"Updating Token with Ticket, success : %@", ((ticket != nil) && (ticket.didSucceed) ? @"YES" : @"NO"));
 	//Response body is the response from twitter server
@@ -296,9 +258,7 @@
 	if ((ticket != nil) && (ticket.didSucceed))
 	{
 		//Response body is the response from twitter server
-		NSString *responseBody = [[NSString alloc]
-								  initWithData:data 
-								  encoding:NSUTF8StringEncoding];
+		NSString *responseBody = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
 		//clean the previous access token available
 		if (accessToken != nil)
 		{
@@ -307,11 +267,9 @@
 		}
 		
 		//allocate the token object with the one just retreived from the server
-		self.accessToken = [[OAToken alloc]
-							initWithHTTPResponseBody:responseBody];
+		self.accessToken = [[OAToken alloc] initWithHTTPResponseBody:responseBody];
 
-		LWE_LOG(@"action token success with key %@ and secret %@", self.accessToken.key, 
-				self.accessToken.secret);
+		LWE_LOG(@"action token success with key %@ and secret %@", self.accessToken.key, self.accessToken.secret);
 		[responseBody release];
 	}
 	else if ((ticket != nil) && (!ticket.didSucceed))
@@ -327,23 +285,15 @@
 }
 
 //! Handy method just used for preparing all of the request, and gives back the prepared request. 
-- (OAMutableURLRequest *)prepareURLForAuthType:(LWETwitterAuthType)lwet 
-									withTicket:(OAServiceTicket *)ticket 
-								  responseData:(NSData *)data
+- (OAMutableURLRequest *)prepareURLForAuthType:(LWETwitterAuthType)lwet withTicket:(OAServiceTicket *)ticket responseData:(NSData *)data
 {
 	[self updateAccessTokenWithTicket:ticket data:data];
 	//request back for user authentication
-	NSURL *url = [NSURL URLWithString:
-				  [NSString stringWithFormat:@"%@/%@", kServerNameSecure, 
-				   [self methodNameForAuthType:lwet]]];
-	
-	OAMutableURLRequest *request = [[[OAMutableURLRequest alloc]
-									 initWithURL:url 
-									 consumer:self.consumer 
-									 token:self.accessToken 
-									 realm:nil
-									 signatureProvider:nil]
-									autorelease];	
+	NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@/%@", kServerNameSecure,[self methodNameForAuthType:lwet]]];
+	OAMutableURLRequest *request = [[[OAMutableURLRequest alloc] initWithURL:url consumer:self.consumer
+                                                                     token:self.accessToken
+                                                                     realm:nil
+                                                         signatureProvider:nil] autorelease];	
 	return request;
 }
 
@@ -369,20 +319,14 @@
 	return result;
 }
 
-#pragma mark -
-#pragma mark Memory Management
+#pragma mark - Plumbing
 
 - (void)dealloc
 {
-	if (authenticationView)
-		[authenticationView release];
-	if (accessToken)
-		[accessToken release];
+  [authenticationView release];
+  [accessToken release];
 	[consumer release];
-	
 	[super dealloc];
 }
-
-#pragma mark -
 
 @end
