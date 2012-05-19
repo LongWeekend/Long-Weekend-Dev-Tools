e7a8a00cf6a9e31147c68dcf215bd270c6117427
Mark Makdad <makdad@gmail.com>
2012-05-19 14:43:21 +0900

    Updated copyright holder - LWE LLC owns the lwe-dev-tools repo.

--- Categories/NSString+LWEResolutionHelpers.h
+++ Categories/NSString+LWEResolutionHelpers.h
@@ -1,6 +1,6 @@
 // NSString+LWEResolutionHelpers.h
 //
-// Copyright (c) 2012 Moneytree
+// Copyright (c) 2012 Long Weekend LLC
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 // associated documentation files (the "Software"), to deal in the Software without restriction,
